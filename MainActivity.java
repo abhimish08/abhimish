0ce7267798701988acfacffcef21c7ca7df2ae39
abhimish08 <63704743+abhimish08@users.noreply.github.com>
2020-04-15 13:12:51 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.stree_plus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+    /** Called when the user taps the Send button */
+
+
+    public void login(View view)
+        startActivity(intent);
+    }
+}
+
